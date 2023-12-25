d48b070ea9783bab58409fc0b2c09e4f9f7f8884
KvashaVladyslav <146590289+KvashaVladyslav@users.noreply.github.com>
2023-12-25 05:07:16 +0100

    upp

---
+++ src/main.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Отримуємо елементи DOM
+    var backdrop = document.getElementById('backdrop');
+    var toggleButton = document.getElementById('toggleButton');
+    // Додаємо слухач подій для кнопки
+    toggleButton.addEventListener('click', function () {
+        // Змінюємо клас backdrop при натисканні кнопки
+        backdrop.classList.toggle('is-open');
+    });
+});
+document.addEventListener('DOMContentLoaded', function () {
+    // Отримуємо елементи DOM
+    var backdrop = document.getElementById('backdrop');
+    var toggleButton = document.getElementById('close-btn');
+    // Додаємо слухач подій для кнопки
+    toggleButton.addEventListener('click', function () {
+        // Видаляємо клас "is-open" при натисканні кнопки
+        backdrop.classList.remove('is-open');
+    });
+});
